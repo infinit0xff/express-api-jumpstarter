cb765070d542256e277da1c649341da9805e9816
infinit0xff <infinit0xff@gmail.com>
2019-08-14 16:17:00 -0700

    initialize database connection in /routes/index.js.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import packageInfo from '../../package.json'
+import initializeDb from '../db';
 	
 let router = express();
 	
-router.use('/',(req, res, next) => {
-    res.status(200).json({
-	    name: packageInfo.name,
-	    description: packageInfo.description,
-	    version: packageInfo.version
-	}); 
+initializeDb(db => {
+	router.use('/',(req, res, next) => {
+		res.status(200).json({
+			name: packageInfo.name,
+			description: packageInfo.description,
+			version: packageInfo.version
+		}); 
+	});
+		
 });
-	
+
 export default router;
