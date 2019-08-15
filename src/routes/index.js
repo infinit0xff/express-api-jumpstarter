c0b5b8e75d7ae5a09b7dd82a36d29e7c729ab11f
infinit0xff <infinit0xff@gmail.com>
2019-08-14 17:56:30 -0700

    added middleware support for /v1 routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import packageInfo from '../../package.json'
 import initializeDb from '../db';
-	
+import middleware from '../middleware';
+import config from '../config';
+
 let router = express();
 	
 initializeDb(db => {
+
+	// internal middleware for '/v1'.
+	router.use(middleware({ config, db }));
+
 	router.use('/',(req, res, next) => {
 		res.status(200).json({
 			name: packageInfo.name,
