ec4c69053a58da87c211e5424a64f263873e3951
infinit0xff <infinit0xff@gmail.com>
2019-08-14 22:07:24 -0700

    use error handling middleware for all routes.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import http from 'http';
 import express from 'express';
 import routes from './routes';
 import config from './config';
+import {
+    notFound,
+    internalError
+} from './middleware/error-handlers';
 
 let app = express();
 app.server = http.createServer(app);
@@ -13,6 +17,9 @@ app.use('/v1', routes);
 app.get('/',(req, res, next) => {
     res.send('API is Alive!');
 });
+
+// error handling middleware returns 404 and 500 respectively.
+app.use(notFound, internalError);
 
 app.server.listen(process.env.PORT || config.port, (err, status) => {
     if(err){
